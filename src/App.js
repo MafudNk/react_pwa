5cdf53005414dab1604b37d27e4e759199dd378f
mafud nur karim <mafudnurkarim@gmail.com>
2021-08-05 21:22:13 +0700

    add components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Browse from './components/Browse';
 import Arraived from './components/Arraived';
+import Clients from './components/Clients';
+import AsideMenu from './components/AsideMenu';
+import Footer from './components/Footer';
 function App() {
   return (
     <>
@@ -10,6 +13,9 @@ function App() {
       <Hero />
       <Browse />
       <Arraived />
+      <Clients />
+      <AsideMenu/>
+      <Footer/>
     </>
   );
 }
